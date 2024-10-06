0a96bffe1edbc0f5c582581e8ad704f304551129
yuqi-yale <yuqi.zhao@yale.edu>
2024-10-05 20:47:05 -0400

    update profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default function Home() {
+  return (
+      <MaxWidthWrapper className='pb-24 pt-10 lg:grid lg:grid-cols-5'>
+        <div className="col-span-4 px-6 lg:px-0 lg:pt-5">
+          aaa
+        </div>
+
+      </MaxWidthWrapper>
+  );
+}
